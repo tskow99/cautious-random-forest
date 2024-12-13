3cb61aecf6f575f26948d5a9b17a57e10a814d3a
davfeldm17 <112324304+davfeldm17@users.noreply.github.com>
2024-12-13 17:30:50 -0500

    Update data.py

--- data.py
+++ data.py
@@ -5,6 +5,11 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+import warnings
+warnings.filterwarnings('ignore')
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 def german_credit_data():
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data"
@@ -34,4 +39,33 @@ def german_credit_data():
     X_train, X_calib, y_train, y_calib = train_test_split(X_train_calib, y_train_calib, test_size=0.1, random_state=42, stratify=y_train_calib)
     return X_train, X_calib,X_test, y_train, y_calib, y_test
 
-
+def heart_disease_data():
+    HD=pd.read_csv('heart_disease_uci.csv')
+    cols_to_drop=['id','dataset']
+    HD_cleaned=HD.drop(columns=cols_to_drop)
+    
+    dummycols=['sex','cp','restecg','slope','thal']
+    HD_cleaned=pd.get_dummies(HD_cleaned,columns=dummycols,drop_first=True)
+    
+    dropcols=['trestbps','fbs','oldpeak']
+    HD_cleaned=HD_cleaned.drop(columns=dropcols)
+    imp = IterativeImputer(max_iter=1000, random_state=0)
+    imp=imp.fit(HD_cleaned)
+    Ximp=imp.transform(HD_cleaned)
+    Ximp=pd.DataFrame(Ximp,columns=HD_cleaned.columns)
+    X=Ximp
+    
+    num_first=X.num
+    num=np.where(num_first>0,1,0)
+    X=X.drop(columns=['num'])
+    kf=KFold(n_splits=10)
+    X_train_calib, X_test, y_train_calib, y_test = train_test_split(X,
+                                                      num,
+                                                      test_size=0.33,
+                                                      random_state=0)
+    X_train, X_calib, y_train, y_calib = train_test_split(X_train_calib,
+                                                          y_train_calib, 
+                                                          test_size=0.1, 
+                                                          random_state=0, 
+                                                          stratify=y_train_calib)
+    return X_train, X_calib,X_test, y_train, y_calib, y_test
